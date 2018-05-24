9696e95415bee3707ca0664e00552d9be0ecff6d
Tyler Roberts <tcroberts@live.ca>
2018-05-24 00:28:09 -0700

    Add __main__

---
+++ weatherterm/core/__main__.py
@@ -0,0 +1,64 @@
+import sys
+from argparse import ArgumentParser
+
+from weatherterm.core import parser_loader, ForecastType, Unit
+
+
+def _validate_forecast_args(args):
+    if args.forecast_option is None:
+        err_msg = ("One of these arguments must be used: "
+                   "-td/--today, -5d/--fivedays, -10d/--tendays, -w/--weekend")
+        print(f'{argparser.prog}: error: {err_msg}',
+              file=sys.stderr)
+        sys.exit()
+
+
+parsers = parser_loader.load("./weatherterm/parsers")
+
+argparser = ArgumentParser(
+    prog="weatherterm",
+    description="Weather info from weather.com on your terminal")
+
+required = argparser.add_argument_group('required arguments')
+
+required.add_argument("-p", "--parser",
+                      choices=parsers.keys(),
+                      required=True,
+                      dest="parser",
+                      help="Specify which parser is going to be used for scraping")
+
+unit_values = [name.title() for name, value in Unit.__members__.items()]
+
+argparser.add_argument("-u", "--unit",
+                       choices=unit_values,
+                       required=False,
+                       dest="unit",
+                       help="Specify the unit to display the temperatures")
+
+required.add_argument("-a", "--areacode",
+                      required=True,
+                      dest="area_code",
+                      help=("The code area to get the weather broadcast from." 
+                            "\nCan be obtained at https://weather.com"))
+
+argparser.add_argument("-v", "--version",
+                       action="version",
+                       version="%(prog)s 1.0")
+
+argparser.add_argument("-td", "--today",
+                       dest="forecast_option",
+                       action="store_const",
+                       const="ForecastType.TODAY",
+                       help="Show the weather forecast for the current day")
+
+args = argparser.parse_args()
+
+_validate_forecast_args(args)
+
+cls = parsers[args.parser]
+
+parser = cls()
+results = parser.run(args)
+
+for result in results:
+    print(results)
