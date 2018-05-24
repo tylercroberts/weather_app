29cc85fea5d3f0c7eea05d1312e88fd8b676987e
Tyler Roberts <tcroberts@live.ca>
2018-05-24 00:41:42 -0700

    Initialize weather parser

---
+++ weatherterm/parsers/weather_com_parser.py
@@ -0,0 +1,26 @@
+from weatherterm.core import ForecastType
+
+def WeatherComParser:
+
+    def __init__(self):
+        self._forecast = {
+            ForecastType.TODAY: self._today_forecast,
+            ForecastType.FIVEDAYS: self._five_and_ten_days_forecast,
+            ForecastType.TENDAYS: self.five_and_ten_days_forecast,
+            ForecastType.WEEKEND:self._weekend_forecast,
+        }
+
+
+    def _today_forecast(self, args):
+        raise NotImplementedError()
+
+    def _five_and_ten_days_forecast(self, args):
+        raise NotImplementedError()
+
+    def _weekend_forecast(self, args):
+        raise NotImplementedError()
+
+    def run(self, args):
+        self._forecast_type = args.forecast_option
+        forecast_function = self._forecast[args.forecast_option]
+        return forecast_function(args)
