bed345409bd3b8ed7a4c7265c3ab8924e3fdd133
Tyler Roberts <tcroberts@live.ca>
2018-05-23 23:24:45 -0700

    Update base_enum & add __init__

--- weatherterm/core/base_enum.py
+++ weatherterm/core/base_enum.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 
 class BaseEnum(Enum):
-    def _generate_next_value_(self, name, start, count, last_value):
+    def _generate_next_value_(name, start, count, last_value):
         return name
